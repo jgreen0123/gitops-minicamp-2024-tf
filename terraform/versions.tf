5bf24b4fe1d2a566e49be5af906e8fcf5110a534
Jamarr <jgreen0123@gmail.com>
2024-11-03 17:57:24 +0000

    fixed formatting in versions file

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,9 +1,10 @@
 terraform {
   required_version = ">= 1.0.0, < 2.0.0"
+
   backend "s3" {
-    bucket = "gitops-tf-backend-rg2017"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
+    bucket         = "gitops-tf-backend-rg2017"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
     dynamodb_table = "GitopsTerraformLocks-rg2017"
   }
 }
