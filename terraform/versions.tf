14e703ab8153f02a6a68e5e6f87042c400c4bf98
Jamarr <jgreen0123@gmail.com>
2024-11-03 09:59:51 -0500

    Added s3 bucket

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,3 +1,9 @@
 terraform {
   required_version = ">= 1.0.0, < 2.0.0"
+  backend "s3" {
+    bucket = "gitops-tf-backend-rg2017"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "GitopsTerraformLocks-rg2017"
+  }
 }
